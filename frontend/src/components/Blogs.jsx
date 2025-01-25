763f6bd669181b0a471af090c9fe049f04c3a9ba
shuazafen <olomoshuaomozafen@gmail.com>
2025-01-25 10:37:13 +0100

    react

---
+++ frontend/src/components/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {} from 'react-icons/fa'
+
+const Blogs = () => {
+  return (
+    <div className='container'>
+       mkla
+    </div>
+  )
+}
+
+export default Blogs
